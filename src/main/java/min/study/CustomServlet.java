15078e16c16e49d37770dbb90ae70722cf52ed81
dhmin5693 <dhmin5693@naver.com>
2021-04-14 01:43:56 +0900

    add spring framework context

--- src/main/java/min/study/CustomServlet.java
+++ src/main/java/min/study/CustomServlet.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
 
 public class CustomServlet extends HttpServlet {
 
@@ -15,6 +17,12 @@ public class CustomServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
+        ApplicationContext context =
+            (ApplicationContext) getServletContext().getAttribute(
+                WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+
+        HelloService helloService = context.getBean(HelloService.class);
+
         System.out.println("doGet");
 
         resp.getWriter().println("<html>");
@@ -22,7 +30,7 @@ public class CustomServlet extends HttpServlet {
         resp.getWriter().println("<body>");
         resp.getWriter().println("<h1>Hello, This is custom servlet.</h1>");
         resp.getWriter().println("<h1>my name is "
-            + getServletContext().getAttribute("name")
+            + helloService.getName()
             + ".</h1>");
         resp.getWriter().println("</body>");
         resp.getWriter().println("</head>");
---
+++ src/main/java/min/study/HelloService.java
@@ -0,0 +1,11 @@
+package min.study;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String getName(){
+        return "MIN";
+    }
+}
