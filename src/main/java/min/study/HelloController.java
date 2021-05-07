15353d10b780ec5071511a7b6ad96e480ae8ca07
dhmin5693 <dhmin5693@naver.com>
2021-05-08 01:24:04 +0900

    set spring mvc bean

--- src/main/java/min/study/HelloController.java
+++ src/main/java/min/study/HelloController.java
@@ -1,17 +1,24 @@
 package min.study;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @Autowired
-    private HelloService helloService;
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello, " + helloService.getName();
+    }
+
+    @GetMapping("/sample")
+    public String sample() {
+        return "WEB-INF/sample.html";
     }
 }
--- src/main/java/min/study/WebConfig.java
+++ src/main/java/min/study/WebConfig.java
@@ -1,10 +1,32 @@
 package min.study;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Configuration
 @ComponentScan
 public class WebConfig {
 
+    @Bean
+    public HandlerMapping handlerMapping() {
+        var handlerMapping = new RequestMappingHandlerMapping();
+//        handlerMapping.setInterceptors();
+        handlerMapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return handlerMapping;
+    }
+
+    @Bean
+    public HandlerAdapter handlerAdapter() {
+        var handlerAdapter = new RequestMappingHandlerAdapter();
+//        handlerAdapter.setArgumentResolvers();
+//        handlerAdapter.setReturnValueHandlers();
+//        handlerAdapter.setMessageConverters();
+        return handlerAdapter;
+    }
 }
